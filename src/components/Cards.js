31a4abb68b0af8f6c7d31eb35bf714b97a01d4df
parisdafermos <parisdafermos@gmail.com>
2022-02-06 17:56:52 +0000

    add countup library and visualise data in cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,18 @@
 import React from "react";
-
-export default () => {
+import "./Cards.css";
+import CountUp from "react-countup";
+export default (props) => {
+  const countryData = props.countryData;
   return (
-    <div>
-      <p>Cards</p>
+    <div className="d-flex justify-content-center ml-5">
+      <div className="card-div-infected mx-3 text-center red-font">
+        <p className="">Infected</p>
+        <CountUp end={countryData.confirmed} duration={2}></CountUp>
+      </div>
+      <div className="card-div-deaths text-center blue-font">
+        <p>Deaths</p>
+        <CountUp end={countryData.deaths} duration={2}></CountUp>
+      </div>
     </div>
   );
 };
